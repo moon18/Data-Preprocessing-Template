cc6983a66201818d6102126fb6a6557e0403b889
Moonis Rasheed <moonisrasheed18@gmail.com>
2019-04-01 15:37:01 +0500

    File Uploaded

---
+++ data_preprocessing_template.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def dataPreprocess(dataset,int(test_size=0.8),int(random_state=0),feature_scaling=False):
+    if ".csv" not in dataset:
+        return("CSV format not applied to dataset, Try again")
+    dataset = pd.read_csv(dataset)
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, len(dataset.columns)-1].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size, random_state)
+    if feature_scaling==True:
+        sc_X = StandardScaler()
+        X_train = sc_X.fit_transform(X_train)
+        X_test = sc_X.transform(X_test)
+        sc_y = StandardScaler()
+        y_train = sc_y.fit_transform(y_train)
+    return X_test,y_test,X_train,y_train
